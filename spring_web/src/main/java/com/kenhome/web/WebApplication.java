96ad05817e303429c1b7f3017cd3ec76b4a4b2fa
chenmeikai <562899487@qq.com>
2018-09-17 11:29:53 +0800

    no message

--- spring_web/src/main/java/com/kenhome/web/WebApplication.java
+++ spring_web/src/main/java/com/kenhome/web/WebApplication.java
@@ -3,7 +3,7 @@ package com.kenhome.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages ="com.kenhome")
 public class WebApplication {
 
     public static void main(String[] args) {
