93cdc90c9af9ebefb63a1843053a613fa5662971
chenmeikai <562899487@qq.com>
2018-09-20 22:56:24 +0800

    管理模块构建 货币处理模块构建

---
+++ spring_server/src/main/java/com/kenhome/server/service/coinApi/BaseCoinApiService.java
@@ -0,0 +1,13 @@
+package com.kenhome.server.service.coinApi;
+
+import com.kenhome.common.model.universal.ResponseModel;
+
+/**
+ * @Author cmk
+ * @Description 货币处理接口基类
+ * @Date 2018\9\20 0020 22:53
+ */
+public interface BaseCoinApiService {
+
+    public ResponseModel recharge();
+}
