b0673dba5fff721077eeced9ff512d16df2aa25c
chenmeikai <562899487@qq.com>
2018-09-27 23:01:45 +0800

    netty配置

--- spring_web/src/main/java/com/kenhome/web/config/netty/NettyServer.java
+++ spring_web/src/main/java/com/kenhome/web/config/netty/NettyServer.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "netty")
 public class NettyServer {
 
---
+++ spring_web/src/main/java/com/kenhome/web/config/netty/ServerChannelHandlerAdapter.java
@@ -0,0 +1,45 @@
+package com.kenhome.web.config.netty;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * NettyServer通道适配器
+ * @author cmk
+ * @date 2018/9/27 - 上午21:40
+ */
+@Component
+@Sharable
+@Slf4j
+public class ServerChannelHandlerAdapter extends SimpleChannelInboundHandler {
+
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Client say : " + msg.toString());
+        //返回客户端消息 - 我已经接收到了你的消息
+        ctx.writeAndFlush("Received your message : " + msg.toString());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("RemoteAddress : " + ctx.channel().remoteAddress() + " active !");
+        ctx.writeAndFlush("连接成功！");
+        super.channelActive(ctx);
+    }
+
+}
